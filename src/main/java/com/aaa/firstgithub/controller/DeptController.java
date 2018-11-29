9b1c9efb66c37e541f7c6657cd9863afeda773ba
aaa521a1314 <1425406645@qq.com>
2018-11-29 09:34:27 +0800

    Initial commit

---
+++ src/main/java/com/aaa/firstgithub/controller/DeptController.java
@@ -0,0 +1,13 @@
+package com.aaa.firstgithub.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:zjf
+ * createTime:2018-11-29 09:080
+ */
+public class DeptController {
+    public static void main(String[] args) {
+        System.out.println("测试GitHub提交");
+    }
+}
